e573d3ddfda70c86092686e044892b7dbeb4b3ac
Vitor Windberg <vitorwlima13@gmail.com>
2024-06-06 00:29:36 -0300

    feat: add real dates to planner

--- src/app/(protected)/planner/page.tsx
+++ src/app/(protected)/planner/page.tsx
@@ -2,13 +2,26 @@
 
 import { AddTaskInput } from '~/app/components/add-task-input'
 import { PageIntroduction } from '~/app/components/page-introduction'
+import {
+  startOfToday,
+  subDays,
+  eachDayOfInterval,
+  addDays,
+  format,
+  isToday,
+} from 'date-fns'
 
 const getTodayAndLastWeekDaysAndNextWeekDays = () => {
-  return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+  const today = startOfToday()
+
+  const startDate = subDays(today, 7)
+  const endDate = addDays(today, 7)
+
+  return eachDayOfInterval({ start: startDate, end: endDate })
 }
 
 const Planner = () => {
-  const todayAndLastWeekAndNextWeek = getTodayAndLastWeekDaysAndNextWeekDays()
+  const dates = getTodayAndLastWeekDaysAndNextWeekDays()
 
   return (
     <main className="flex h-screen flex-col overflow-hidden p-6">
@@ -25,14 +38,15 @@ const Planner = () => {
           })
         }}
       >
-        {todayAndLastWeekAndNextWeek.map((date) => (
-          <div key={date} className="w-72 min-w-72 p-4">
+        {dates.map((date) => (
+          <div key={format(date, 'dd/MM/yyyy')} className="w-72 min-w-72 p-4">
             <header className="mb-2 flex flex-col">
-              <p className="text-2xl font-semibold">Monday</p>
-              <p className="text-sm font-light text-neutral-800">January 11</p>
+              <p className="text-2xl font-semibold">{format(date, 'EEEE')}</p>
+              <p className="text-sm font-light text-neutral-800">
+                {format(date, 'MMMM d')} {isToday(date) ? '(Today)' : ''}
+              </p>
             </header>
-
-            <AddTaskInput id={`task-${date}`} />
+            <AddTaskInput id={`task-${format(date, 'dd/MM/yyyy')}`} />
           </div>
         ))}
       </div>
