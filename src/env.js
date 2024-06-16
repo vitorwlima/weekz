ea97a6ef9ae314c61ce5fbd80d7c05fa083edc81
Vitor Windberg <vitorwlima13@gmail.com>
2024-06-16 00:01:21 -0300

    feat: add non pooling db url

--- src/env.js
+++ src/env.js
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
+    DATABASE_URL_NON_POOLING: z.string().url().optional(),
     CLERK_SECRET_KEY: z.string(),
     NODE_ENV: z
       .enum(['development', 'test', 'production'])
@@ -30,6 +31,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL_NON_POOLING: process.env.DATABASE_URL_NON_POOLING,
     NODE_ENV: process.env.NODE_ENV,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
